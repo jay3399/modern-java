fc44126a2924e0e5e37ec68806801d27f1854ed0
jay <joswxx@gmail.com>
2023-06-16 19:30:54 +0900

    람다 생성자참조  , 오토박싱기능 메모리주의 -> 참조타입말고 원시타입지원 인터페이스또한 존재한다.

---
+++ src/main/java/com/example/modernjava/Block.java
@@ -0,0 +1,22 @@
+package com.example.modernjava;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+import java.util.function.IntFunction;
+
+public class Block {
+
+  private int weight;
+
+  public Block(int weight) {
+    this.weight = weight;
+  }
+
+  public int getWeight() {
+    return weight;
+  }
+
+
+
+}
--- src/main/java/com/example/modernjava/ModernJavaApplication.java
+++ src/main/java/com/example/modernjava/ModernJavaApplication.java
@@ -2,9 +2,14 @@ package com.example.modernjava;
 
 import static com.example.modernjava.Color.*;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+import java.util.function.IntFunction;
+import java.util.function.Supplier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -17,6 +22,9 @@ public class ModernJavaApplication {
 		List<Apple> apples = Arrays.asList(new Apple(130, RED), new Apple(170, GREEN),
 				new Apple(190, RED),
 				new Apple(110, RED));
+
+
+
 
 
 		Thread hello = new Thread(
@@ -61,7 +69,63 @@ public class ModernJavaApplication {
 		prettyApple.prettyPrintAppleV2(apples, apple -> System.out.println(apple.getWeight()));
 
 
+//    생성자 참조 만든뒤 , 사용한다
+
+//    디폴트 생성자참조
+//		Supplier<Block> blockSupplier = Block::new;
+//
+//		Block block = blockSupplier.get();
+
+
+
+//    무게포함 생성자
+
+		IntFunction<Block> blockIntFunction = Block::new;
+
+		// int -> Integer 오토박싱 , 메모리 소모 .
+		Function<Integer, Block> blockFunction = Block::new;
+
+		Block apply = blockIntFunction.apply(30);
+
+		Block apply1 = blockFunction.apply(20);
+
+		System.out.println("apply.getWeight() = " + apply.getWeight());
+
+		System.out.println("apply1 = " + apply1.getWeight());
+
+		List<Integer> weights = Arrays.asList(3, 2, 10, 7);
+
+		List<Block> map = map(weights, Block::new);
+
+		for (Block block : map) {
+
+			System.out.println("block = " + block.getWeight());
+
+		}
+
+		BiFunction<Integer, Enum, Apple> aNew = Apple::new;
+
+		Apple apply2 = aNew.apply(20, GREEN);
+
+		System.out.println("apply2 = " + apply2.getWeight());
+		System.out.println("apply2.getColor( = " + apply2.getColor());
 
 	}
+
+
+	public static List<Block> map(List<Integer> list, Function<Integer,Block> f) {
+
+		List<Block> result = new ArrayList<>();
+
+		for (Integer i : list) {
+			result.add(f.apply(i));
+		}
+
+		return result;
+
+
+	}
+
+
 
 }
